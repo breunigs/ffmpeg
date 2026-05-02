d31101ae04c33a9d15e7a3e5c4b9cc89fb1d46bf
Stefan Breunig <stefan-ffmpeg-devel@breunig.xyz>
2026-05-02 08:01:45 +0200

    avfilter/vf_frei0r: move filter_frame before config_link_props
    Preparatory step: reorder functions so filter_frame appears before
    config_link_props. No functional change.
    
    Signed-off-by: Stefan Breunig <stefan-ffmpeg-devel@breunig.xyz>

--- libavfilter/vf_frei0r.c
+++ libavfilter/vf_frei0r.c
@@ -316,6 +316,50 @@ static av_cold void uninit(AVFilterContext *ctx)
         dlclose(s->dl_handle);
 }
 
+static int filter_frame(AVFilterLink *inlink, AVFrame *in)
+{
+    Frei0rContext *s = inlink->dst->priv;
+    AVFilterLink *outlink = inlink->dst->outputs[0];
+    /* align parameter is the line alignment, not the buffer alignment.
+     * frei0r expects line size to be width*4 so we want an align of 1
+     * to ensure lines aren't padded out. */
+    AVFrame *out = ff_default_get_video_buffer2(outlink, outlink->w, outlink->h, 1);
+    int ret;
+    if (!out)
+        goto fail;
+
+    ret = av_frame_copy_props(out, in);
+    if (ret < 0) {
+        av_frame_free(&out);
+        return ret;
+    }
+
+    if (in->linesize[0] != out->linesize[0]) {
+        AVFrame *in2 = ff_default_get_video_buffer2(outlink, outlink->w, outlink->h, 1);
+        if (!in2)
+            goto fail;
+        av_frame_copy(in2, in);
+        if (av_frame_copy_props(in2, in) < 0) {
+            av_frame_free(&in2);
+            goto fail;
+        }
+        av_frame_free(&in);
+        in = in2;
+    }
+
+    s->update(s->instance, in->pts * av_q2d(inlink->time_base),
+                   (const uint32_t *)in->data[0],
+                   (uint32_t *)out->data[0]);
+
+    av_frame_free(&in);
+
+    return ff_filter_frame(outlink, out);
+fail:
+    av_frame_free(&in);
+    av_frame_free(&out);
+    return AVERROR(ENOMEM);
+}
+
 static int config_link_props(AVFilterLink *outlink)
 {
     AVFilterContext *ctx = outlink->src;
@@ -365,50 +409,6 @@ static int query_formats(const AVFilterContext *ctx,
         return AVERROR(ENOMEM);
 
     return ff_set_common_formats2(ctx, cfg_in, cfg_out, formats);
-}
-
-static int filter_frame(AVFilterLink *inlink, AVFrame *in)
-{
-    Frei0rContext *s = inlink->dst->priv;
-    AVFilterLink *outlink = inlink->dst->outputs[0];
-    /* align parameter is the line alignment, not the buffer alignment.
-     * frei0r expects line size to be width*4 so we want an align of 1
-     * to ensure lines aren't padded out. */
-    AVFrame *out = ff_default_get_video_buffer2(outlink, outlink->w, outlink->h, 1);
-    int ret;
-    if (!out)
-        goto fail;
-
-    ret = av_frame_copy_props(out, in);
-    if (ret < 0) {
-        av_frame_free(&out);
-        return ret;
-    }
-
-    if (in->linesize[0] != out->linesize[0]) {
-        AVFrame *in2 = ff_default_get_video_buffer2(outlink, outlink->w, outlink->h, 1);
-        if (!in2)
-            goto fail;
-        av_frame_copy(in2, in);
-        if (av_frame_copy_props(in2, in) < 0) {
-            av_frame_free(&in2);
-            goto fail;
-        }
-        av_frame_free(&in);
-        in = in2;
-    }
-
-    s->update(s->instance, in->pts * av_q2d(inlink->time_base),
-                   (const uint32_t *)in->data[0],
-                   (uint32_t *)out->data[0]);
-
-    av_frame_free(&in);
-
-    return ff_filter_frame(outlink, out);
-fail:
-    av_frame_free(&in);
-    av_frame_free(&out);
-    return AVERROR(ENOMEM);
 }
 
 static av_cold int filter_init(AVFilterContext *ctx)
